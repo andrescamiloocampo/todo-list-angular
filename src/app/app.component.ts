ad7901f79f6570b2d1d671b789322e2c9ec06b8e
Andres Camilo Ocampo Ramirez <andres_ocampo82211@elpoli.edu.co>
2024-11-18 14:48:11 -0500

    Chore ( Components ): Implement todo list (main)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { TaskComponent } from "./task/task.component";
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone: true,  
+  imports: [ TaskComponent,FormsModule,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'todo-list';
+  title = 'To do App';
+
+  tasks: {name: string,completed: boolean}[] = [];
+  newTask: string = '';
+
+  addTask(){
+    
+    if(this.newTask.trim()){
+      this.tasks.push({name: this.newTask,completed:false});
+      this.newTask = '';
+    }      
+
+    console.log(this.tasks)
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+    console.log(this.tasks)
+  }
+
+  clear(){
+    this.tasks = this.tasks.filter(task => task.completed === false);    
+  }
 }
---
+++ src/app/task/task.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-task',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './task.component.html',
+  styleUrl: './task.component.css'
+})
+export class TaskComponent {  
+  @Input() name = '';
+  @Input() completed = false;
+}
